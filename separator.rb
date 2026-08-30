0fa72f44040a03fdf75eb163aa59b3d4b6df1726
hxmbl <180758703+hxmbl@users.noreply.github.com>
2026-08-30 23:59:43 +0100

    Update separator formula

--- separator.rb
+++ separator.rb
@@ -8,44 +8,10 @@ class Separator < Formula
   version "1.0.0"
   license "MIT"
 
-  livecheck do
-    url :homepage
-    regex(/^v(\d+(?:\.\d+)+)$/i)
-    strategy :github_latest
-  end
-
-  on_macos do
-    if Hardware::CPU.intel?
-      def install
-        system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-        system "make", "-C", "build"
-        bin.install "build/separator"
-      end
-    end
-    if Hardware::CPU.arm?
-      def install
-        system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-        system "make", "-C", "build"
-        bin.install "build/separator"
-      end
-    end
-  end
-
-  on_linux do
-    if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      def install
-        system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-        system "make", "-C", "build"
-        bin.install "build/separator"
-      end
-    end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      def install
-        system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-        system "make", "-C", "build"
-        bin.install "build/separator"
-      end
-    end
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "make", "-C", "build"
+    bin.install "build/separator"
   end
 
   test do
